28ecf0f767fa2bbd052bf497893b19d370569715
as6620 <as6620@bs.amalnet.k12.il>
2025-03-16 13:38:03 +0200

    credits

--- app/src/main/java/com/example/internalfiles/MainActivity.java
+++ app/src/main/java/com/example/internalfiles/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.internalfiles;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -24,6 +28,42 @@ public class MainActivity extends AppCompatActivity {
         eT = (EditText) findViewById(R.id.eT);
     }
 
+    public void goSave(View view) {
+    }
+
+    public void goReset(View view) {
+    }
+
+    public void goExit(View view) {
+    }
 
 
+    /**
+     * Inflates the options menu.
+     *
+     * @param menu The options menu.
+     * @return true if the menu is created successfully.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * Handles selection of menu items.
+     *
+     * @param item The selected menu item.
+     * @return true if the menu item was handled successfully.
+     */
+    @Override
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menuCred) {
+            Intent si = new Intent(this, activity_credits.class);
+            startActivity(si);
+        }
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/internalfiles/activity_credits.java
@@ -0,0 +1,24 @@
+package com.example.internalfiles;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class activity_credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+    }
+
+    public void goClose(View view) {
+        finish();
+    }
+}
