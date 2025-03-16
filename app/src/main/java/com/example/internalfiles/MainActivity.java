8c26860ebe9c64c202339008df1ff0b264539fd6
as6620 <as6620@bs.amalnet.k12.il>
2025-03-16 13:28:13 +0200

    xml

--- app/src/main/java/com/example/internalfiles/MainActivity.java
+++ app/src/main/java/com/example/internalfiles/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.internalfiles;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tV;
+    EditText eT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tV = (TextView) findViewById(R.id.tV);
+        eT = (EditText) findViewById(R.id.eT);
     }
+
+
+
 }
